eef444bbcfee00c78b36bef3504ecad930662eef
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-09 16:01:30 +0200

    v-for done

--- js/script.js
+++ js/script.js
@@ -6,34 +6,34 @@
 
 function createVue() {
      new Vue ({
-          el: '#myVue',
+          el: '#vueContainer',
 
           data: {
                contacts: [
-                    { name: 'Michele', avatar: '_1', visible: true, messages: [
+                    { name: 'Judy Alvarez', avatar: '_1', visible: true, messages: [
                                                                                 { date: '10/01/2020 15:30:55', text: 'Hai portato a spasso il cane?', status: 'sent'},
                                                                                 { date: '10/01/2020 15:50:00', text: 'Ricordati di dargli da mangiare', status: 'sent'},
-                                                                                { date: '10/01/2020 16:15:22', text: 'Tutto fatto!', status: 'received'}
+                                                                                { date: '10/01/2020 16:15:22', text: 'Abbandona questa guerra... rimani con me', status: 'received'}
                                                                               ],
                     },
 
-                    { name: 'Fabio', avatar: '_2', visible: true, messages: [
+                    { name: 'Misty Olszewsky', avatar: '_2', visible: true, messages: [
                                                                                 { date: '20/03/2020 16:30:00', text: 'Ciao come stai?', status: 'sent'},
                                                                                 { date: '20/03/2020 16:30:55', text: 'Bene grazie! Stasera ci vediamo?', status: 'received'},
-                                                                                { date: '20/03/2020 16:35:00', text: 'Mi piacerebbe ma devo andare a fare la spesa.', status: 'sent'}
+                                                                                { date: '20/03/2020 16:35:00', text: "Misty, stanotte ho sognato l'imperatrice..", status: 'sent'}
                                                                             ],
                     },
                     
-                    { name: 'Samuele', avatar: '_3', visible: true, messages: [
+                    { name: 'Panam Palmer', avatar: '_3', visible: true, messages: [
                                                                                 { date: '28/03/2020 10:10:40', text: 'La Marianna va in campagna', status: 'received'},
                                                                                 { date: '28/03/2020 10:20:10', text: 'Sicuro di non aver sbagliato chat?', status: 'sent'},
-                                                                                { date: '28/03/2020 16:15:22', text: 'Ah scusa!', status: 'received'}
+                                                                                { date: '28/03/2020 16:15:22', text: 'Comunque vada, con te fino alla fine... choom', status: 'received'}
                                                                               ],
                     },
 
-                    { name: 'Luisa', avatar: '_4', visible: true, messages: [
+                    { name: 'Johnny Silverhand', avatar: '_4', visible: true, messages: [
                                                                                 { date: '10/01/2020 15:30:55', text: 'Lo sai che ha aperto una nuova pizzeria?', status: 'sent'},
-                                                                                { date: '10/01/2020 15:50:00', text: 'Si, ma preferirei andare al cinema', status: 'received'},
+                                                                                { date: '10/01/2020 15:50:00', text: "V, non c'è piu tempo! L'Arasaka sta arrivando.", status: 'received'},
                                                                                 ],
                     },                    
                 ]
